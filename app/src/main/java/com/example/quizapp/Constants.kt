4ca37af5f42d9ca202d87c3a768bbac5e98491de
Luke <luke.badawy@concentrix.com>
2021-06-11 16:12:56 +1000

    Further progress on the Quiz app

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,79 @@
+package com.example.quizapp
+
+object Constants {
+    fun getQuestions(): ArrayList<Question> {
+        var questionList = ArrayList<Question>()
+
+        val question1 =
+            Question(
+                1,
+                "Which video game hero is this",
+                R.drawable.ic_question_image_link,
+                "Zelda",
+                "Link",
+                "Ganon",
+                "Hyrule",
+                2
+            )
+
+        questionList.add(question1)
+
+        val question2 =
+            Question(
+                1,
+                "Which company owns this logo",
+                R.drawable.ic_question_image_xbox,
+                "Xbox",
+                "Sony",
+                "Microsoft",
+                "Nintendo",
+                3
+            )
+
+        questionList.add(question2)
+
+        val question3 =
+            Question(
+                1,
+                "Who is beshulet",
+                R.drawable.ic_question_image_question_mark2,
+                "John",
+                "Adele",
+                "Natalie",
+                "Roman",
+                3
+            )
+
+        questionList.add(question3)
+
+        val question4 =
+            Question(
+                1,
+                "What is the capital city of Switzerland",
+                R.drawable.ic_question_image_question_mark2,
+                "Zalbamont",
+                "Uster",
+                "Berlin",
+                "Zulric",
+                4
+            )
+
+        questionList.add(question4)
+
+        val question5 =
+            Question(
+                1,
+                "How many rotations per year does earths moon make",
+                R.drawable.ic_question_image_question_mark2,
+                "The moon does not rotate",
+                "364",
+                "1",
+                "700",
+                1
+            )
+
+        questionList.add(question5)
+
+        return questionList
+    }
+}
--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quizapp
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btn_start.setOnClickListener {
+            if(text_input_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name.", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
